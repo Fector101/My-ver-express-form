faa88f6a468bd1bdcf45cf38de6aeca3fa78ab13
Fector101 <109623702+Fector101@users.noreply.github.com>
2024-08-23 13:11:05 +0100

    Add files via upload
    This is my initial upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express= require('express')
+require('dotenv').config()
+const nodemailer=require('nodemailer')
+console.log(process.env.EMAIL_USER,process.env.EMAIL_PASS)
+let transporter=nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+
+let mailOptions = {
+    from: 'piperham007@gmail.com',
+    to: 'fabianjoseph063@gmail.com',
+    subject: 'Test Email',
+    text: 'Hello, this is a test email sent from Node.js!'
+  };
+  
+  // Send the email
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+const app = express()
+const port =3000
+app.use(express.static('public'))
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/index.html')
+})
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
