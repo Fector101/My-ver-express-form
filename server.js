3f720b7da0b37bd6d8a9bc44b6c8398bb97b48b4
Fector101 <109623702+Fector101@users.noreply.github.com>
2024-08-23 13:51:54 +0100

    Update server.js
    Other function that test's link of summer to DOM

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express= require('express')
 require('dotenv').config()
 const nodemailer=require('nodemailer')
 console.log(process.env.EMAIL_USER,process.env.EMAIL_PASS)
+function test(){
 let transporter=nodemailer.createTransport({
     service: 'gmail',
     auth:{
@@ -26,13 +27,23 @@ let mailOptions = {
       console.log('Email sent: ' + info.response);
     }
   });
-
+}
 const app = express()
 const port =3000
+const cors = require('cors');
+app.use(cors());
+function getData() {
+  return { message: "Hello from the server! batman" };
+}
 app.use(express.static('public'))
+app.get('/api/data', (req, res) => {
+  const data = getData();
+  res.json(data);
+});
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html')
 })
+
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
 })
