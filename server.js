98f56820b30c4dc3b23b1d2c5f0e263d9968a285
Fector101 <109623702+Fector101@users.noreply.github.com>
2024-08-25 04:18:31 +0100

    Update server.js
    :) forgot to import cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const nodemailer=require('nodemailer')
 const formParser=require('body-parser')
+const cors=require('cors')
 const app = express()
 const port =3000
 
