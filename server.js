f54b7d6721fdc83673e3209427e14a0223a1329f
Fector101 <109623702+Fector101@users.noreply.github.com>
2024-08-25 07:51:31 +0100

    Update server.js
    added html edit  with showing user name feature and removed redundancies

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const formParser=require('body-parser')
 const cors=require('cors')
 const app = express()
 const port =3000
-email_html=`
+const email_html=(user_name) =>`
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,6 +17,12 @@ email_html=`
       margin: 0;
       padding: 0;
     }
+    h1{
+        line-height: 40px;
+    }
+    *{
+      box-sizing: border-box;
+    }
     .email-container {
       background-color: #ffffff;
       padding: 20px;
@@ -28,6 +34,7 @@ email_html=`
     .email-header {
       text-align: center;
       padding: 20px;
+      padding-bottom: 10px;
       background-color: #007bff;
       color: white;
       border-top-left-radius: 10px;
@@ -36,6 +43,7 @@ email_html=`
     .email-body {
       padding: 20px;
       text-align: center;
+      background-color: #007bff1e;
     }
     .email-body h2 {
       color: #333;
@@ -47,7 +55,7 @@ email_html=`
     .email-footer {
       text-align: center;
       padding: 20px;
-      background-color: #f4f4f4;
+      background-color: #d3d7dc;
       border-bottom-left-radius: 10px;
       border-bottom-right-radius: 10px;
     }
@@ -63,29 +71,50 @@ email_html=`
     .socials {
       margin-top: 30px;
     }
-    .socials img {
-      width: 40px;
+    .socials a{
+      display: inline-block;
+    }
+    .socials svg{
+      overflow: visible;
+      --size:36px;
+      width: var(--size);
+      height: var(--size);
       margin: 0 10px;
+
+    }
+    .socials svg.whatsapp{
+      --fill-color:rgb(44, 202, 44);
+      fill: var(--fill-color);
+      background-color: white;
+      padding: 5px;   
+      border-radius: 60%;
     }
   </style>
 </head>
 <body>
   <div class="email-container">
     <div class="email-header">
-      <h1>Thank You for Registering!</h1>
+      <h1>Fector 101 <br> You for Registering!</h1>
     </div>
     <div class="email-body">
-      <h2>Welcome to [Company Name]!</h2>
-      <p>Your registration is now confirmed. You can start using our services by logging into your account.</p>
-      <a href="https://yourwebsite.com/login" class="button">Go to Dashboard</a>
+      <!-- <h2>Welcome to [Company Name]!</h2> -->
+      <p>Your registration has been now confirmed.</p>
+      <!-- <a href="https://yourwebsite.com/login" class="button">Go to Dashboard</a> -->
     </div>
     <div class="email-footer">
       <p>Follow us on social media for the latest updates!</p>
       <div class="socials">
-        <a href="https://facebook.com/yourcompany"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook"></a>
-        <a href="https://twitter.com/yourcompany"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter"></a>
-        <a href="https://instagram.com/yourcompany"><img src="https://cdn-icons-png.flaticon.com/512/733/733558.png" alt="Instagram"></a>
-        <a href="https://linkedin.com/company/yourcompany"><img src="https://cdn-icons-png.flaticon.com/512/733/733561.png" alt="LinkedIn"></a>
+        <a href="https://wa.me/2348112321825?text=Hi%20Fabian%2c%20I%27d%20like%20to%20">
+          <svg class="whatsapp" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
+        </a>
+
+        <a href="https://github.com/Fector101" alt="Instagram">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>
+        </a>
+        
+        <a href="https://x.com/OPieMonarch">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+        </a>
       </div>
       <p>If you have any questions, feel free to <a href="mailto:support@yourcompany.com">contact us</a>.</p>
     </div>
@@ -94,7 +123,7 @@ email_html=`
 </html>
 `
 
-async function sendMail(send_to) {
+async function sendMail(send_to,user_name) {
   try {
     let transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -108,7 +137,7 @@ async function sendMail(send_to) {
       to: send_to,
       from: 'fabianjoseph063@gmail.com',
       subject: 'Test Email',
-      html: email_html
+      html: email_html(user_name)
       //text: 'Hello, this is a test email sent from Node.js!'
     };
 
@@ -143,7 +172,7 @@ app.get("/success", (req, res) => {
 });
 
 app.post("/submit", async(req, res) => {
-    const sent_bool = await sendMail(req.body.email)
+    const sent_bool = await sendMail(req.body.email,req.body['user-name'])
     sent_bool?res.sendFile(__dirname+'/public/successful.html'):res.sendFile(__dirname+'/public/failure.html')
 });
 
